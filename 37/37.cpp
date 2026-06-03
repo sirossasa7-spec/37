25cd35a7a953edca4bf0d8aec1f2e6a4f0de1c8a
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-06-03 23:37:32 +0200

    main

--- 37/37.cpp
+++ 37/37.cpp
@@ -1,29 +1,39 @@
 ﻿#include <iostream>
 #include "WebBrowser.h"
+#include "PostfixCalculator.h"
 
 int main()
 {
-    WebBrowser browser;
+    //WebBrowser browser;
 
-    browser.visit("google.com");
-    browser.visit("youtube.com");
-    browser.visit("github.com");
-    browser.visit("openai.com");
+    //browser.visit("google.com");
+    //browser.visit("youtube.com");
+    //browser.visit("github.com");
+    //browser.visit("openai.com");
 
-    browser.showCurrentPage();
+    //browser.showCurrentPage();
 
-    std::cout << "\nBack -> "
-        << browser.back() << std::endl;
+    //std::cout << "\nBack -> "
+    //    << browser.back() << std::endl;
 
-    std::cout << "Back -> "
-        << browser.back() << std::endl;
+    //std::cout << "Back -> "
+    //    << browser.back() << std::endl;
 
-    browser.showCurrentPage();
+    //browser.showCurrentPage();
 
-    std::cout << "\nForward -> "
-        << browser.forward() << std::endl;
+    //std::cout << "\nForward -> "
+    //    << browser.forward() << std::endl;
 
-    browser.showCurrentPage();
+    //browser.showCurrentPage();
+
+    std::string expression;
+
+    std::cout << "Enter postfix expression:\n";
+    std::getline(std::cin, expression);
+
+    std::cout << "Result = "
+        << PostfixCalculator::calculate(expression)
+        << std::endl;
 
     return 0;
 }
